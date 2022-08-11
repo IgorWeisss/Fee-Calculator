37c30c24d43ee6766f391294821abbc889acd05e
Igor - Ubuntu <igorweiss@hotmail.com.br>
2022-08-11 16:31:16 -0300

    added calc method

--- src/components/CalcProcess.tsx
+++ src/components/CalcProcess.tsx
@@ -1,5 +1,61 @@
-export function CalcProcess (props: any) {
+import { formatNumber } from "../pages/Calculator"
+
+interface CalcProps {
+  totalValue: number,
+  numberOfInstallments: number
+}
+
+const calc = {
+  spotFee: 0.0299,
+  termFee: 0.0349,
+  transactionFee: 0.70,
+  anticipationFeeRate: 0.0199,
+  creditFee: function (totalValue: number, numberOfInstallments: number) {
+    return numberOfInstallments ==1 ? totalValue * this.spotFee : totalValue * this.termFee
+  },
+  anticipationFee: function (totalValue: number, numberOfInstallments: number) {
+    let totalFee = 0
+  
+    for (let i = numberOfInstallments; i > 1; i--) {
+  
+      let netValue = (totalValue - this.creditFee(totalValue, numberOfInstallments))/numberOfInstallments
+      let res = netValue * ((i - 1) * this.anticipationFeeRate)
+  
+      totalFee += res
+    }
+  
+    return totalFee 
+  },
+  totalFee: function (totalValue: number, numberOfInstallments: number) {
+    return this.transactionFee + this.creditFee(totalValue, numberOfInstallments) + this.anticipationFee(totalValue, numberOfInstallments)
+  },
+  calculateNetValue: function (totalValue: number, numberOfInstallments: number) {
+    return Number((totalValue - this.totalFee(totalValue, numberOfInstallments)).toFixed(2))
+  },
+  calculateNetValueWithoutFees: function (recievedValue: number, numberOfInstallments: number) {
+  
+    let totalValue = recievedValue
+    let totalRecieved = this.calculateNetValue(totalValue, numberOfInstallments)
+  
+    while (totalRecieved < recievedValue) {
+      totalValue += 0.01
+      totalRecieved = this.calculateNetValue(totalValue, numberOfInstallments)
+    }
+  
+    return Number(totalValue.toFixed(2))
+  
+  }
+}
+
+export function CalcProcess ({ totalValue, numberOfInstallments }: CalcProps) {
+
+  if (totalValue == 0) return <></>
+  let res = calc.calculateNetValueWithoutFees(totalValue, numberOfInstallments)
+
   return (
-    <h1>{props.number}</h1>
+    <div className="mt-8">
+      <p>O cliente paga <strong>{formatNumber(res)}</strong></p>
+      <p> em {numberOfInstallments} parcelas de <strong>{formatNumber(res/numberOfInstallments)}</strong></p>
+    </div>
   )
 }
--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -2,22 +2,23 @@ import logo from '../assets/logo.png'
 import { useState } from 'react';
 import { CalcProcess } from '../components/CalcProcess';
 
-function formatNumber (children: any) { 
+export function formatNumber (children: number) { 
   return "R$ " + children.toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+\,)/g, '$1.');
 }
 
 
 export function Calculator () {
   
-  const [formatedNumber, setFormatedNumber] = useState("R$0,00")
   const [number, setNumber] = useState(0)
+  const [formatedNumber, setFormatedNumber] = useState("R$0,00")
 
   return (
     <div className="flex h-screen w-screen justify-center items-center p-4 bg-teal-600">
       <div className="bg-white h-fit w-full md:max-w-3xl box-border rounded-2xl p-6 flex items-center justify-center flex-col">
         <img src={logo} alt="Logotipo da Carol Mueller Fotografia" className='w-20 mb-2' />
         <h1 className='font-medium text-xl'>Calculadora de taxas</h1>
-        <input 
+        <input
+          className='text-center font-bold text-3xl' 
           type='text'
           inputMode='numeric'
           value={formatedNumber}
@@ -29,7 +30,7 @@ export function Calculator () {
           }}
         />
 
-        <CalcProcess number={number}/>
+        <CalcProcess totalValue={number} numberOfInstallments={10}/>
       </div>
     </div>
   )
